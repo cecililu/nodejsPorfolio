562c49e9bf61edbc4f912817847626f1931d10d0
cecil <cecilghimire@gmail.com>
2022-08-15 17:19:50 +0545

    port

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.get('/contact',(req,res,next)=>{
     res.render('contact')
 })
  const port=process.env.PORT 
-app.listen(port,'127.0.0.1',()=>{
+app.listen(port,()=>{
     console.log("listening to requesto on port 8000")
 })
