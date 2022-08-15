214a41fc959b2939f25f5610bf04b09523748aeb
cecil <cecilghimire@gmail.com>
2022-08-15 20:20:39 +0545

    seperated code

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-const fs = require("fs");
+const fs =require("fs");
 const express = require("express");
-const { dirname } = require("path");
 const app= express();
 app.use(express.static(__dirname+'/public'))
+app.use(express.json())
 app.set('view engine','ejs')
 
-
-
+//geting json from another file
 const blog=JSON.parse(
     fs.readFileSync(`${__dirname}/blog.json`)
     );
 
-
-app.get('/blog',(req ,res ,next)=>{
-  res.status(200).render('about',{data:blog})
-})
-
-app.get('/home',(req,res,next)=>{
-    res.render('home')
-})
-
-app.get('/contact',(req,res,next)=>{
+//handler function
+//api
+const getblog=(req,res)=>{
+    res.status(200).json(blog)
+}
+//get blog page
+const blogpage=(req ,res )=>{
+    res.status(200).render('about',{ data: blog })
+}
+//get home page
+const homepage=(req,res)=>{
+    res.status(200).render('home')
+}
+//contact page
+const contactpage=(req,res)=>{
     res.render('contact')
-})
- const port=process.env.PORT 
+}
+
+
+
+//routes
+app
+    .route('/v1/blogs')
+    .get(getblog)
+
+app
+    .route('/blog').get(blogpage)
+
+app
+    .route('/home').get(homepage)
+
+app
+    .route('/contact')
+    .get(contactpage)
+
+//server
+const port=process.env.PORT ||8000
 app.listen(port,()=>{
     console.log("listening to requesto on port 8000")
 })
